797d854ac8c523f7904e4ecb6921acede185e3a0
Viktor Oreshkin <imselfish@stek29.rocks>
2024-10-10 14:39:16 +0300

    add NFQWS_DEFAULT_ARGS

--- config/zapret.sh
+++ config/zapret.sh
@@ -1,6 +1,6 @@
 # shellcheck shell=bash
 # shellcheck disable=SC2034
 NFQWS_INSTANCES=(
-    "--debug --qnum=200 --dpi-desync=split2 --dpi-desync-split-pos=2"
-    "--debug --qnum=210 --dpi-desync=fake --dpi-desync-repeats=6"
+    "${NFQWS_DEFAULT_ARGS:-} --qnum=200 --dpi-desync=split2 --dpi-desync-split-pos=2"
+    "${NFQWS_DEFAULT_ARGS:-} --qnum=210 --dpi-desync=fake --dpi-desync-repeats=6"
 )
