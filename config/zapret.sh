a9f25a2f3b3c598c161d8e503a6b0f2c84f3a9a5
Viktor Oreshkin <imselfish@stek29.rocks>
2024-12-07 21:34:22 +0300

    update zapret config

--- config/zapret.sh
+++ config/zapret.sh
@@ -1,6 +1,6 @@
 # shellcheck shell=bash
 # shellcheck disable=SC2034
 NFQWS_INSTANCES=(
-    "${NFQWS_DEFAULT_ARGS:-} --qnum=200 --dpi-desync=split2 --dpi-desync-split-pos=2"
-    "${NFQWS_DEFAULT_ARGS:-} --qnum=210 --dpi-desync=fake --dpi-desync-repeats=6"
+    "${NFQWS_DEFAULT_ARGS:-} --qnum=200 --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls=/opt/zapret/files/fake/tls_clienthello_www_google_com.bin"
+    "${NFQWS_DEFAULT_ARGS:-} --qnum=210 --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-any-protocol"
 )
