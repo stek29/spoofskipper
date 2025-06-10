855e4f5dce17c8c2daaee5e1f05bcafe10ac6946
Viktor Oreshkin <imselfish@stek29.rocks>
2025-06-11 00:12:32 +0300

    new-zapret: quic, discord

--- config/zapret.sh
+++ config/zapret.sh
@@ -8,4 +8,5 @@ NFQWS_ARGS=(
 NFQWS_INSTANCES=(
     "--filter-tcp=80,443 --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls=/opt/zapret/files/fake/tls_clienthello_www_google_com.bin"
     "--filter-udp=443 --dpi-desync=fake --dpi-desync-repeats=6"
+    "--filter-udp=50000-50100 --filter-l7=discord,stun --dpi-desync=fake --dpi-desync-repeats=6"
 )
