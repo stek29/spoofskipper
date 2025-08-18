af41c03cdeab768c87bfc924a66d6a03ef63edd2
Viktor Oreshkin <imselfish@stek29.rocks>
2025-08-19 00:08:40 +0300

    upd config

--- config/zapret.sh
+++ config/zapret.sh
@@ -6,7 +6,7 @@ NFQWS_ARGS=(
 
 # shellcheck disable=SC2034
 NFQWS_INSTANCES=(
-    "--filter-tcp=80,443 --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls-mod=rnd,rndsni,padencap"
+    "--filter-tcp=80,443 --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls-mod=rnd,rndsni,padencap --wssize 1:6"
     "--filter-udp=443 --dpi-desync=fake --dpi-desync-repeats=6"
     "--filter-udp=50000-50100 --filter-l7=discord,stun --dpi-desync=fake --dpi-desync-repeats=6"
 )
